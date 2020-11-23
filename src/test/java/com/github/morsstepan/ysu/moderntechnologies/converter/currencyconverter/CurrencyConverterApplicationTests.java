5f430cc0a4edebc51b4593a062072d3981832ebd
Stepan Morozov <morsstepan@icloud.com>
2020-11-23 20:37:43 +0300

    Spring boot scheme

---
+++ src/test/java/com/github/morsstepan/ysu/moderntechnologies/converter/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.morsstepan.ysu.moderntechnologies.converter.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
